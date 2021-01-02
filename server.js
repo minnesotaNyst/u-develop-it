def5c02dbf08583c536ac82921f816ba2b7728f7
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-01-02 14:53:06 -0600

    added routes to the server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,43 +26,105 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 //  This method runs the SQL query and executes the callback with all the resulting rows that match the query.
-// !uncomment this out later in the module
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-// 	console.log(rows);
-// });
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+	const sql = `SELECT * FROM candidates`;
+	const params = [];
+	db.all(sql, params, (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message });
+			return;
+		}
+
+		res.json({
+			message: 'success',
+			data: rows
+		});
+	});
+});
 
 // GET a single candidate
 // selecting a single candidate by their primary key is the only way to ensure the candidate requested is the one that's returned.
-// !uncomment this out later in the module
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-// 	if (err) {
-// 		console.log(err);
-// 	}
-// 	console.log(row);
-// });
+// This property is an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available as req.params.name. In this case, we are using /:id so it is available as params.id.
+app.get('/api/candidate/:id', (req, res) => {
+	const sql = `SELECT * FROM candidates 
+				 WHERE id = ?`;
+	const params = [req.params.id];
+	db.get(sql, params, (err, row) => {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+
+		res.json({
+			message: 'success',
+			data: row
+		});
+	});
+});
 
 // Delete a candidate
 // The question mark (?) denotes a placeholder, making this a prepared statement. Prepared statements can have placeholders that can be filled in dynamically with real values at runtime.
 // An additional param argument can provide values for prepared statement placeholders. Here, we're hardcoding 1 temporarily to demonstrate how prepared statements work. If we need additional placeholders, the param argument can be an array that holds multiple values.
+app.delete('/api/candidate/:id', (req, res) => {
+	const sql = `DELETE FROM candidates WHERE id = ?`;
+	const params = [req.params.id];
+	db.run(sql, params, function (err, result) {
+		if (err) {
+			res.status(400).json({ error: res.message });
+			return;
+		}
+
+		res.json({
+			message: 'successfully deleted',
+			changes: this.changes
+		});
+	});
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+	const errors = inputCheck(
+		body,
+		'first_name',
+		'last_name',
+		'industry_connected'
+	);
+	if (errors) {
+		res.status(400).json({ error: errors });
+		return;
+	}
+
+	const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+	const params = [body.first_name, body.last_name, body.industry_connected];
+	// ES5 function, not arrow function, to use `this`
+	db.run(sql, params, function (err, result) {
+		if (err) {
+			res.status(400).json({ error: err.message });
+			return;
+		}
+
+		res.json({
+			message: 'success',
+			data: body,
+			id: this.lastID
+		});
+	});
+});
+
+// Create a candidate
 // !uncomment this out later in the module
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function (err, result) {
 // 	if (err) {
 // 		console.log(err);
 // 	}
-// 	console.log(result, this, this.changes);
+// 	console.log(result, this.lastID);
 // });
-
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function (err, result) {
-	if (err) {
-		console.log(err);
-	}
-	console.log(result, this.lastID);
-});
 
 // Default response for any other request(Not Found) Catch all
 // !make sure this is the LAST route because it will override all of the others
